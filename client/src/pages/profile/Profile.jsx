3a0f05e47773e9bb5b4bd99d713e50139d739635
Marko <tomicmark3@gmail.com>
2023-01-06 17:38:05 +0100

    design finished

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,7 +1,71 @@
 import "./profile.scss";
-
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../components/posts/Posts";
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className='profile'>
+      <div className='images'>
+        <img
+          src='https://images.pexels.com/photos/14487514/pexels-photo-14487514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt=''
+          className='cover'
+        />
+        <img
+          src='https://images.pexels.com/photos/14212900/pexels-photo-14212900.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+          alt=''
+          className='profilePic'
+        />
+      </div>
+      <div className='profileContainer'>
+        <div className='uInfo'>
+          <div className='left'>
+            <a href='http://facebook.com'>
+              <FacebookTwoToneIcon />
+            </a>
+            <a href='http://facebook.com'>
+              <InstagramIcon fontSize='medium' />
+            </a>
+            <a href='http://facebook.com'>
+              <TwitterIcon fontSize='medium' />
+            </a>
+            <a href='http://facebook.com'>
+              <LinkedInIcon fontSize='medium' />
+            </a>
+            <a href='http://facebook.com'>
+              <PinterestIcon fontSize='medium' />
+            </a>
+          </div>
+          <div className='center'>
+            <span>Jane Doe</span>
+            <div className='info'>
+              <div className='item'>
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+              <div className='item'>
+                <LanguageIcon />
+                <span>site.dev</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className='right'>
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
